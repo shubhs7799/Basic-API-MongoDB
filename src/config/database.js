29870c0c63aa394866a5d06bda05c54bfbf25647
Your Name <rshubham928@gmail.com>
2025-01-11 14:34:52 +0530

    Delete all files

--- src/config/database.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://rshubham928:5d0X7x2NUO9QUiFL@learnbackend.ubacu.mongodb.net/devTinder');
-}
-
-module.exports = connectDB
